32f44ff14d6297a0ef01b0374a83b6b4e29984a9
init1-dev <init1.dev@gmail.com>
2024-04-02 18:16:27 +0200

    ADD: route to generate public access key

--- controllers/loginController.ts
+++ controllers/loginController.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { generateAccessToken } from "../util/generateAccessToken";
+import { generateAccessToken, generatePublicToken } from "../util/generateAccessToken";
 
 export const loginController = express.Router();
 
 loginController.post('/createNewUser', (req, res) => {
     const token = generateAccessToken(req.body.username);
     
+    res.json(token);
+})
+
+loginController.get('/PUBLIC_ACCESS_KEY', (_req, res) => {
+    const token = generatePublicToken();
+
     res.json(token);
 })
--- util/generateAccessToken.ts
+++ util/generateAccessToken.ts
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "./getSecretKey";
 
-const TEN_YEARS = 31536000;
+const ANIO_IN_MS = 365.25 * 24 * 60 * 60 * 1000;
+const TEN_YEARS = ANIO_IN_MS * 10;
 
 export const generateAccessToken = (username: string) => {
-    return jwt.sign({username}, SECRET_KEY, { expiresIn: TEN_YEARS });
+    return jwt.sign({username}, SECRET_KEY, { expiresIn: ANIO_IN_MS });
+}
+
+export const generatePublicToken = (username = null) => {
+    return jwt.sign({username}, null, { expiresIn: TEN_YEARS, algorithm: "none" });
 }
