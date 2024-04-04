7e6cf645726fddc7a6d289f56796b80b9a77e265
init1-dev <init1.dev@gmail.com>
2024-04-04 21:34:42 +0200

    ADD: new documentation using swagger, FIX: minor folder structure improve

--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
 import express, { NextFunction, Request, Response } from 'express';
-import path from 'path';
 
 import { rootRoutes } from './routes/root';
 import { roomsRoutes } from './routes/rooms';
@@ -14,9 +13,7 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/css', express.static(path.resolve(process.cwd(), 'css')));
-app.use('/js', express.static(path.resolve(process.cwd(), 'js')));
-app.use('/images', express.static(path.resolve(process.cwd(), 'images')));
+app.use(express.static('public'));
 
 app.use((req: Request, _res: Response, next: NextFunction) => {
     console.log('REQUEST LOGGER', req.method, req.url);
@@ -33,7 +30,10 @@ app.use('/employees', authMiddleware, employeesRoutes);
 app.use('/messages', authMiddleware, messagesRoutes);
 
 app.use((_req: express.Request, res: express.Response, _next: express.NextFunction) => {
-    res.status(404).send();
+    res.status(404).json({
+        status: 404,
+        message: "Invalid path, please refer to the documentation to see available routes."
+    });
 });
 
 app.use((err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
--- routes/root.ts
+++ routes/root.ts
@@ -11,12 +11,3 @@ rootRoutes.get('/', async(_req: Request, res: Response) => {
         res.status(500).json(error);
     }
 })
-
-rootRoutes.get('/results', async(_req: Request, res: Response) => {
-    try {   
-        res.sendFile(path.resolve(process.cwd(), 'results.html'));
-    } catch (error) {
-        console.error('An error ocurred', error);
-        res.status(500).json(error);
-    }
-})
