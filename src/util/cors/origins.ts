6c38f47b2792a2b12abd431b66213a9b5da681d9
init1-dev <init1.dev@gmail.com>
2024-06-04 16:17:36 +0200

    ADD: cypress test url

--- src/util/cors/origins.ts
+++ src/util/cors/origins.ts
@@ -1,5 +1,6 @@
 export const origins = [
     'http://localhost:3000',
     'http://localhost:5173',
+    'http://192.168.2.168:5173/',
     'http://hotel-miranda-dashboard-init.s3-website.eu-west-3.amazonaws.com'
 ]
